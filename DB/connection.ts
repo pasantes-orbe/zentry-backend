a85ee7e5e00bcbc67606282deaf0a54aeebc22cb
Alejandro Chicala <chicalaale6@gmail.com>
2022-10-07 14:57:07 -0300

    prod test1

--- DB/connection.ts
+++ DB/connection.ts
@@ -1,8 +1,15 @@
 import { Sequelize } from 'sequelize';
 
+// LOCAL
 const db = new Sequelize('Countries', 'postgres', 'admin', {
     host: 'localhost',
     dialect: 'postgres',
 });
+
+// PRODUCTION
+// const db = new Sequelize('qegbbqka', 'qegbbqka', 's0BlHlZEfD1gPRUqxtAc1IMMVFkxTGMy', {
+//     host: 'kesavan.db.elephantsql.com',
+//     dialect: 'postgres',
+// });
 
 export default db;
--- dist/DB/connection.js
+++ dist/DB/connection.js
@@ -1,9 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
+// LOCAL
 const db = new sequelize_1.Sequelize('Countries', 'postgres', 'admin', {
     host: 'localhost',
     dialect: 'postgres',
 });
+// PRODUCTION
+// const db = new Sequelize('qegbbqka', 'qegbbqka', 's0BlHlZEfD1gPRUqxtAc1IMMVFkxTGMy', {
+//     host: 'kesavan.db.elephantsql.com',
+//     dialect: 'postgres',
+// });
 exports.default = db;
 //# sourceMappingURL=connection.js.map
