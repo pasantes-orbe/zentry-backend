ca03c99e17af104cd89cc3aca2a0b1e8cd8eca41
Alejandro Chicala <contacto@orbesoftware.com.ar>
2023-02-09 08:52:30 -0300

    a

--- DB/connection.ts
+++ DB/connection.ts
@@ -6,11 +6,10 @@ let db: Sequelize;
 
 // LOCAL
 try {
-    db = new Sequelize('railway', 'postgres', 'K4bKPagGIjAVGQ5TOZby', {
-        host: 'containers-us-west-164.railway.app',
-        dialect: 'postgres',
-        port: 7068
-    });
+     db = new Sequelize('Countries', 'admin', 'admin', {
+         host: 'localhost',
+         dialect: 'postgres',
+     });
  } catch (error) {
      throw new Error("No se pudo conectar con la base de datos")
  }
