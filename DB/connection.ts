a8e155ac7da6d942d9414c8a718ec6f14ef0094f
Santiago Beron <santiagobdea@gmail.com>
2024-01-03 15:24:16 -0300

    Orbe APP backend

--- DB/connection.ts
+++ DB/connection.ts
@@ -4,34 +4,33 @@ import { Sequelize } from 'sequelize';
 
 let db: Sequelize;
 
-console.log(process.env.PORT);
 
 
-    // LOCAL
-    try {
-        db = new Sequelize('postgres', 'admindb', 'bu38kuZUpa', {
-            host: 'localhost',
-            dialect: 'postgres',
-            dialectOptions: {
-                useUTC: false, // -->Add this line. for reading from database
-            },
-            timezone: "+05:30"
-        });
-    } catch (error) {
-        throw new Error("No se pudo conectar con la base de datos")
-    }
+    // // LOCAL
+    // try {
+    //     db = new Sequelize('postgres', 'admindb', 'bu38kuZUpa', {
+    //         host: 'localhost',
+    //         dialect: 'postgres',
+    //         dialectOptions: {
+    //             useUTC: false, // -->Add this line. for reading from database
+    //         },
+    //         timezone: "+05:30"
+    //     });
+    // } catch (error) {
+    //     throw new Error("No se pudo conectar con la base de datos")
+    // }
 
-// PRODUCTION
-/*try {
-    db = new Sequelize('railway', 'postgres', 'K4bKPagGIjAVGQ5TOZby', {
-         host: 'containers-us-west-164.railway.app',
+// PRODUCTION ORBE COUNTRIES
+try {
+    db = new Sequelize('railway', 'postgres', 'dFAE11e-21451Age5AeFgF6612Gba3cF', {
+         host: 'viaduct.proxy.rlwy.net',
          dialect: 'postgres',
-         port: 7068
+         port: 35230 
      });
 } catch (error) {
     throw new Error("No se pudo conectar con la base de datos")
 }
-*/
+
 
 
 
--- dist/DB/connection.js
+++ dist/DB/connection.js
@@ -2,32 +2,30 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 let db;
-console.log(process.env.PORT);
-// LOCAL
+// // LOCAL
+// try {
+//     db = new Sequelize('postgres', 'admindb', 'bu38kuZUpa', {
+//         host: 'localhost',
+//         dialect: 'postgres',
+//         dialectOptions: {
+//             useUTC: false, // -->Add this line. for reading from database
+//         },
+//         timezone: "+05:30"
+//     });
+// } catch (error) {
+//     throw new Error("No se pudo conectar con la base de datos")
+// }
+// PRODUCTION ORBE COUNTRIES
 try {
-    db = new sequelize_1.Sequelize('postgres', 'admindb', 'bu38kuZUpa', {
-        host: 'localhost',
+    db = new sequelize_1.Sequelize('railway', 'postgres', 'dFAE11e-21451Age5AeFgF6612Gba3cF', {
+        host: 'viaduct.proxy.rlwy.net',
         dialect: 'postgres',
-        dialectOptions: {
-            useUTC: false, // -->Add this line. for reading from database
-        },
-        timezone: "+05:30"
+        port: 35230
     });
 }
 catch (error) {
     throw new Error("No se pudo conectar con la base de datos");
 }
-// PRODUCTION
-/*try {
-    db = new Sequelize('railway', 'postgres', 'K4bKPagGIjAVGQ5TOZby', {
-         host: 'containers-us-west-164.railway.app',
-         dialect: 'postgres',
-         port: 7068
-     });
-} catch (error) {
-    throw new Error("No se pudo conectar con la base de datos")
-}
-*/
 /*
  try {
      db = new Sequelize('Countries', 'postgres', 'admin', {
