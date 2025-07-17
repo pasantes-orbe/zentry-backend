0bc1a8ecc644794474d27ff98a5bbfd5e63474d7
anacesia <ana@orbesoftware.com.ar>
2025-07-17 15:34:16 -0300

    Limpiar archivos generados (.js) y respetar .gitignore

--- dist/classes/Upload.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cloudinary_1 = require("cloudinary");
-cloudinary_1.v2.config(process.env.CLOUDINARY_URL);
-class Upload {
-}
-exports.default = Upload;
-//# sourceMappingURL=Upload.js.map
--- dist/routes/owners.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/', (req, res) => res.json("HOLAMUNDO"));
-exports.default = router;
-//# sourceMappingURL=owners.routes.js.map
