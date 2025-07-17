ef4b817d24c60e93808439cab5dae28dfdf82277
anacesia <ana@orbesoftware.com.ar>
2025-07-17 15:49:32 -0300

    Corregir tipado en validateJWT.middleware para extraer uid correctamente

--- middlewares/jwt/validateJWT.middleware.ts
+++ middlewares/jwt/validateJWT.middleware.ts
@@ -1,38 +1,39 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import User from "../../models/user.model";
 
-async function validateJWT(req:Request, res:Response, next:NextFunction) {
-
+async function validateJWT(req: Request, res: Response, next: NextFunction) {
     const token = req.header("Authorization");
 
-    if(!token){
+    if (!token) {
         return res.status(401).json({
             msg: "No hay token de autorización"
         });
     }
 
     try {
+        // Declaramos el tipo explícitamente para que TypeScript no se queje
+        const decoded = jwt.verify(token, "SUPER_SECRET_PASSWORD") as JwtPayload;
 
-        const { uid }: string = jwt.verify( token, "SUPER_SECRET_PASSWORD" );
+        // uid puede venir como string o number, depende de cómo guardaste el token
+        // Por eso hacemos casting a string para usarlo con findByPk
+        const uid = decoded.uid as string;
 
         const user = await User.findByPk(uid);
 
-        if(!user){
+        if (!user) {
             return res.status(404).send({
-                "msg": "El usuario no existe"
-            })
+                msg: "El usuario no existe"
+            });
         }
-        
+
         next();
-        
+
     } catch (error) {
         return res.status(403).send({
-            "msg": "Token inválido"
-        })
+            msg: "Token inválido"
+        });
     }
-
-   
 }
 
 export default validateJWT;
